f4880747a09ef6ef83580963609858e5e504f9d5
RanjithS581 <149576877+RanjithS581@users.noreply.github.com>
2024-05-10 15:59:46 +0530

    passwd.js

--- password strength indicator/passwd.js
+++ password strength indicator/passwd.js
@@ -1,36 +1,53 @@
-document.getElementById('passwd').addEventListener('input', function() {
+var passwd = document.getElementById('passwd')
+passwd.addEventListener('input', function() {
     var password = this.value;
     var c1 = document.querySelector('.c1');
     var c2 = document.querySelector('.c2');
     var c3 = document.querySelector('.c3');
     var output = document.getElementById('output');
-
+    // var p21 = document.querySelector('.p21');
     
-    c1.style.backgroundColor = 'lightslategray';
-    c2.style.backgroundColor = 'lightslategray';
-    c3.style.backgroundColor = 'lightslategray';
+
     output.textContent = '';
+    // p21.style.border = '1px solid blue';
+    
 
-
-    if (/^[A-Z]+$/.test(password)) {
+    if ((password.length > 2 && /[A-Za-z]+/.test(password)) ){
         c1.style.backgroundColor = 'red';
         output.textContent = 'Your password is too weak';
+        output.style.color ='red'
     }
     
 
-    if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    if ((password.length > 2 && /[A-Za-z]+/.test(password)) && /[!@#$%^&*(),.?":{}|<>]/.test(password)) {
         c1.style.backgroundColor = 'red';
-        c2.style.backgroundColor = 'yellow';
-        if (output.textContent === '') {
-            output.textContent = 'Your password is medium';
-        }
+        c2.style.backgroundColor = 'yellow'; 
+        output.textContent = 'Your password is medium';
+        output.style.color ='yellow'
+    
     }
 
+
     
-    if (/[A-Z]/.test(password) && /[a-z]/.test(password) && /[0-9]/.test(password) && /[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    if ((password.length > 2 && /[A-Za-z]+/.test(password)) && /[!@#$%^&*(),.?":{}|<>]/.test(password) && /[0-9]/.test(password) ) {
         c1.style.backgroundColor = 'red';
         c2.style.backgroundColor = 'yellow';
         c3.style.backgroundColor = 'green';
         output.textContent = 'Your password is good';
+        output.style.color ='green'
+        passwd.style.border = '2px solid green'
+    }
+
+});
+
+document.getElementById('label').addEventListener('click', function(){
+    var passwd = document.getElementById('passwd');
+    if(passwd.type == 'password'){
+        passwd.type = 'text';
+        label.textContent = 'Hide'
+    }
+    else{
+        passwd.type = 'password';
+        label.textContent = 'Show';
     }
 });
